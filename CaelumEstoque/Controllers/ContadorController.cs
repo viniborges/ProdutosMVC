0dd21c7860731c88914dbbde494ab8ba7cd938a5
viniborges <viniborges@yahoo.com.br>
2018-06-05 22:12:50 -0300

    Adicionar arquivos de projeto.

---
+++ CaelumEstoque/Controllers/ContadorController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CaelumEstoque.Controllers
+{
+    public class ContadorController : Controller
+    {
+        // GET: Contador
+        public ActionResult Index()
+        {
+            object valor = Session["contador"];
+            int cont = Convert.ToInt32(valor);
+            cont++;
+            Session["Contador"] = cont;
+            return View(cont);
+        }
+    }
+}
